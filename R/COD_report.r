7f724050f6558cc0f4468e9177342ff5a6acb652
Tom Hardwicke <hardwickete@gmail.com>
2017-02-27 21:29:05 -0800

    added ANI error in function example

--- R/COD_report.r
+++ R/COD_report.r
@@ -2,13 +2,14 @@
 #'
 #' This is a helper function for building standardised final outcoems for COD reproducibility reports.
 #' @param Article_ID Enter the article's unique ID code
+#' @param Absence_Of_Necessary_Information_Errors Enter the number of Absence Of Necessary Information Errors
 #' @param Decision_Errors Enter the number of decision errors
 #' @param Major_Numerical_Errors Enter the number of major numerical errors
 #' @param Minor_Numerical_Errors Enter the number of minor numerical errors
 #' @return Returns a formatted table (via kable) reporting error tallys. Also writes out a csv object containing the error tallys.
 #' @export
 #' @examples
-#' COD_Report(Article_ID = "ABhgyo", Decision_Errors = 1, Major_Numerical_Errors = 4, Minor_Numerical_Errors = 12)
+#' COD_Report(Article_ID = "ABhgyo", Absence_Of_Necessary_Information_Errors = 0, Decision_Errors = 1, Major_Numerical_Errors = 4, Minor_Numerical_Errors = 12)
 
 COD_Report <- function(Article_ID, Absence_Of_Necessary_Information_Errors, Decision_Errors, Major_Numerical_Errors, Minor_Numerical_Errors){
   if(Decision_Errors > 0 | Major_Numerical_Errors > 0 | Absence_Of_Necessary_Information_Errors > 0){
