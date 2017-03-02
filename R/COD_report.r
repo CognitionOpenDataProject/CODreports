caf74cb29704b47abb1b64b820d1a2724fd1be62
Tom Hardwicke <hardwickete@gmail.com>
2017-03-02 14:56:24 -0800

    report func handles pilot vs joint

--- R/COD_report.r
+++ R/COD_report.r
@@ -1,6 +1,7 @@
 #' COD_Report function
 #'
-#' This is a helper function for building standardised final outcoems for COD reproducibility reports.
+#' This is a helper function for building standardised final outcomes for COD reproducibility reports.
+#' @param Report_Type Enter 'pilot' or 'joint'
 #' @param Article_ID Enter the article's unique ID code
 #' @param Insufficient_Information_Errors Enter the number of Insufficient Information Errors
 #' @param Decision_Errors Enter the number of decision errors
@@ -9,9 +10,9 @@
 #' @return Returns a formatted table (via kable) reporting error tallys. Also writes out a csv object containing the error tallys.
 #' @export
 #' @examples
-#' COD_Report(Article_ID = "ABhgyo", Insufficient_Information_Errors = 0, Decision_Errors = 1, Major_Numerical_Errors = 4, Minor_Numerical_Errors = 12)
+#' COD_Report(Report_Type = "pilot", Article_ID = "ABhgyo", Insufficient_Information_Errors = 0, Decision_Errors = 1, Major_Numerical_Errors = 4, Minor_Numerical_Errors = 12)
 
-COD_Report <- function(Article_ID, Insufficient_Information_Errors, Decision_Errors, Major_Numerical_Errors, Minor_Numerical_Errors){
+COD_Report <- function(Report_Type, Article_ID, Insufficient_Information_Errors, Decision_Errors, Major_Numerical_Errors, Minor_Numerical_Errors){
   if(Decision_Errors > 0 | Major_Numerical_Errors > 0 | Insufficient_Information_Errors > 0){
     finalOutcome <- "Failure"
   }else{
@@ -24,7 +25,7 @@ COD_Report <- function(Article_ID, Insufficient_Information_Errors, Decision_Err
                              "Minor_Numerical_Errors" = Minor_Numerical_Errors,
                              "Final_Outcome" = finalOutcome)
 
-  filename <- paste("reportObject_", Article_ID, ".csv")
+  filename <- paste("reportObject_", Report_Type, "_", Article_ID, ".csv")
   write.csv(reportObject, filename, row.names = F)
   return(kable(reportObject))
 }
